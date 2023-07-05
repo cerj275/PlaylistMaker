9da0487fb21a4aa454206410e5f5632c579d0a48
cerj275 <cerj275@gmail.com>
2023-07-05 21:41:03 +0300

    Sprint 10

---
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,28 @@
+package com.example.playlistmaker
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val cover: ImageView = itemView.findViewById(R.id.imageViewCover)
+    private val trackName: TextView = itemView.findViewById(R.id.textViewTrackName)
+    private val artistName: TextView = itemView.findViewById(R.id.textViewArtistName)
+    private val trackTime: TextView = itemView.findViewById(R.id.textViewTrackTime)
+
+    fun bind(item: Track) {
+        Glide.with(itemView)
+            .load(item.artworkUrl100)
+            .placeholder(R.drawable.ic_placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(20))
+            .into(cover)
+        trackName.text = item.trackName
+        artistName.text = item.artistName
+        trackTime.text = item.trackTime
+    }
+}
